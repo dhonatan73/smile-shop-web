08d25620ac275bf89651d716791473f9cea17f81
henrideanton <henriqantoni@gmail.com>
2023-11-30 20:21:39 -0300

    adicionada janela descrição produto

--- scripts.js
+++ scripts.js
@@ -6,13 +6,24 @@ const arrayTest = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18
 const totalProducts = productListArray
 console.log(totalProducts)
 
+
+
+function openWindow(name,imagesrc){
+    let productWindow = document.querySelector(".window-division")
+    let list=`<div class="box-window"><div class="header-box"><p>${name}</p><button onClick="closeWindow()" class="close-window">X</button></div>
+    <img class="big-image-box" src="./assets/products/${imagesrc}">
+    </div>`
+
+    productWindow.innerHTML = list
+}
+
 function printList(list, imagelist) {
 
     let listBox = document.querySelector(".product-list")
     let listPrint=''
 
     list.forEach((element, index) => {
-        listPrint+=`<div class="product-box">
+        listPrint+=`<div onClick="openWindow('${element.productName}','${imagelist[index].img}')" class="product-box">
         <img class="image-box" src="./assets/products/${imagelist[index].img}">
         <p class="description">${element.productName}</p>
         <p class="price">R$ ${element.productPrice},00</p>
